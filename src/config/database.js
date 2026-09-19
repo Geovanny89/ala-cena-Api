317cbdf057bd74ff455d855e3bf9a7c3b99a468e
ctCucuta <ctecnologico64@gmail.com>
2026-09-19 11:32:25 -0500

    se ajusta conexion db

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT) || 5432,
+    port: parseInt(process.env.DATABASE_URL),
     dialect: process.env.DB_DIALECT || 'postgres',
     logging: false,
     dialectOptions:
